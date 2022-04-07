002fb1f35346c78b011541a32bd9ac4415615f46
yuping <>
2022-04-07 13:26:07 +0800

    登录页面

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,27 @@
 import axios from 'axios'
+import { Message } from 'element-ui'
 // 创建axios实例
-const service = axios.create()
+const service = axios.create({
+  // 如果执行 npm run dev  值为 /api 正确  /api 这个代理只是给开发环境配置的代理
+  // 如果执行 npm run build 值为 /prod-api  没关系  运维应该在上线的时候 给你配置上 /prod-api的代理
+  baseURL: process.env.VUE_APP_BASE_API,
+  timeout: 5000
+})
 // 请求拦截器
 service.interceptors.request.use()
 // 响应拦截器
-service.interceptors.response.use()
+service.interceptors.response.use(
+  response => {
+    const { success, data, message } = response.data
+    if (success) {
+      return data
+    } else {
+      Message.error(message)
+      return Promise.reject(new Error(message))
+    }
+  }, error => {
+    Message.error(error)
+    return Promise.reject(error)
+  }
+)
 export default service
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,17 +3,17 @@
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <img src="@/assets/common/login-logo.png" alt="">
       </div>
 
-      <el-form-item prop="username">
+      <el-form-item prop="mobile">
         <span class="svg-container">
           <svg-icon icon-class="user" />
         </span>
         <el-input
           ref="username"
-          v-model="loginForm.username"
-          placeholder="Username"
+          v-model="loginForm.mobile"
+          placeholder="请输入手机号"
           name="username"
           type="text"
           tabindex="1"
@@ -30,7 +30,7 @@
           ref="password"
           v-model="loginForm.password"
           :type="passwordType"
-          placeholder="Password"
+          placeholder="请输入密码"
           name="password"
           tabindex="2"
           auto-complete="on"
@@ -41,11 +41,11 @@
         </span>
       </el-form-item>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
+      <el-button class="loginBtn" :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
 
       <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: any</span>
+        <span style="margin-right:20px;">账号: 13800000002</span>
+        <span> 密码: 123456</span>
       </div>
 
     </el-form>
@@ -53,33 +53,33 @@
 </template>
 
 <script>
-import { validUsername } from '@/utils/validate'
-
+import { validMobile } from '@/utils/validate'
+import {mapActions} from 'vuex'
 export default {
   name: 'Login',
   data() {
-    const validateUsername = (rule, value, callback) => {
-      if (!validUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
-      } else {
-        callback()
-      }
+    const validateMobile = (rule, value, callback) => {
+      //
+      validMobile(value) ? callback() : callback(new Error('手机号格式不正确'))
     }
-    const validatePassword = (rule, value, callback) => {
-      if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
-      } else {
-        callback()
-      }
-    }
+    // const validatePassword = (rule, value, callback) => {
+    //   if (value.length < 6) {
+    //     callback(new Error('The password can not be less than 6 digits'))
+    //   } else {
+    //     callback()
+    //   }
+    // }
     return {
       loginForm: {
-        username: 'admin',
-        password: '111111'
+        mobile: '13800000002',
+        password: '123456'
       },
+      // validator 自定义函数
       loginRules: {
-        username: [{ required: true, trigger: 'blur', validator: validateUsername }],
-        password: [{ required: true, trigger: 'blur', validator: validatePassword }]
+        mobile: [{ required: true, trigger: 'blur', message: '手机号不能为空' },
+          { validator: validateMobile, trigger: 'blur' }],
+        password: [{ required: true, trigger: 'blur', message: '密码不能为空' },
+          { min:6, max:16, message: '密码应在6-16位之间', trigger: 'blur' }]
       },
       loading: false,
       passwordType: 'password',
@@ -95,6 +95,7 @@ export default {
     }
   },
   methods: {
+    ...mapActions(['user/login']),
     showPwd() {
       if (this.passwordType === 'password') {
         this.passwordType = ''
@@ -106,21 +107,24 @@ export default {
       })
     },
     handleLogin() {
-      this.$refs.loginForm.validate(valid => {
-        if (valid) {
-          this.loading = true
-          this.$store.dispatch('user/login', this.loginForm).then(() => {
-            this.$router.push({ path: this.redirect || '/' })
-            this.loading = false
-          }).catch(() => {
-            this.loading = false
-          })
-        } else {
-          console.log('error submit!!')
-          return false
+      this.$refs.loginForm.validate(async isOk => {
+        try {
+          if (isOk) {
+            this.loading = true
+            // 运用async的目的是异步转化同步（路由跳转会先行于执行这个方法）
+            await this['user/login'](this.loginForm)
+            this.$router.push('/')
+          }
+        } catch (error) {
+          console.log(error)
+        } finally {
+          // 无论执行try还是catch都会进入finally
+          this.loading = false
         }
       })
     }
+  },
+  computed: {
   }
 }
 </script>
@@ -141,6 +145,8 @@ $cursor: #fff;
 
 /* reset element-ui css */
 .login-container {
+  background-image: url("~@/assets/common/login.jpg");
+  background-position: center;
   .el-input {
     display: inline-block;
     height: 47px;
@@ -165,9 +171,18 @@ $cursor: #fff;
 
   .el-form-item {
     border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
+    background: rgba(255, 255, 255, 0.7); // 输入登录表单的背景色
     border-radius: 5px;
     color: #454545;
+  }
+  .el-form-item__error{
+    color:#fff;
+  }
+  .loginBtn{
+    background: #407ffe;
+    height: 64px;
+    line-height: 32px;
+    font-size: 24px;
   }
 }
 </style>
@@ -175,7 +190,7 @@ $cursor: #fff;
 <style lang="scss" scoped>
 $bg:#2d3a4b;
 $dark_gray:#889aa4;
-$light_gray:#eee;
+$light_gray:#68b0fe;
 
 .login-container {
   min-height: 100%;
@@ -214,6 +229,7 @@ $light_gray:#eee;
 
   .title-container {
     position: relative;
+    bottom:20px;
 
     .title {
       font-size: 26px;
